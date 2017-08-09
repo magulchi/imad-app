aff6513a55eefaa2fa3fe798b4a780dd3f016a0b
magulchi <30774641+magulchi@users.noreply.github.com>
2017-08-09 10:56:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML= 'new html';
 var img = document.getElementById('akshay');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft +'px';
+}
 img.onclick = function()
 {
-    img.style.marginLeft='100px';
+    var interval= setInterval(moveRight,100);
+    //img.style.marginLeft='100px';
 };
