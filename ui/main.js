26182ddc2588b2cce86d9f3f1cc330860f1c9ba5
magulchi <30774641+magulchi@users.noreply.github.com>
2017-08-09 10:41:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML= 'new html';
+var img = document.getElementById('akshay');
+img.onclick = function()
+{
+    img.style.marginleft='100px';
+};
