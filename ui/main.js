d26896cb5c4b9c63cd38da6c1020a939a254b689
magulchi <30774641+magulchi@users.noreply.github.com>
2017-08-10 15:00:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,15 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit= document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names=['name1','name2','name3','name4'];
+     var request=new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+    if(request.readyState === XMLHttpRequest.DONE)
+    {
+        if(request.status === 200)
+        {
+         var names= request.responseText;
+         names=JSON.parse(names);
     var list='';
     for(var i=0; i< names.length; i++)
     {
@@ -30,4 +38,11 @@ submit.onclick = function(){
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+    }
+    }
+    };
+
+  request.open('GET', 'http://akshayama466.imad.hasura-app.io/subnit-name?name=' +name ,true);
+  request.send(null);
+   
 };
